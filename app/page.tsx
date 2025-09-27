a37aa3b468ad693369c997f03af15de028e0c6c2
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-09-27 12:14:04 +0530

    Develop User page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 'use client'
 import {useRouter} from "next/navigation";
-import React, {useState} from "react";
+import {useState} from "react";
+
 
 export default function LoginPage() {
     const router = useRouter();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleLogin(e: React.FormEvent) {
+    function handleLogin(e: React.FocusEvent) {
         e.preventDefault();
+        // check username password, token manage
         router.push("/dashboard");
     }
 
     return (
         <div className="w-full max-w-sm bg-white rounded-xl shadow p-6 space-y-4">
-            <h1 className="text-2xl text-black font-bold">Login</h1>
+            <h1 className="text-2xl font-bold">Login</h1>
 
             <form onSubmit={handleLogin} className="space-y-4">
                 <input type="email" placeholder={"Email Address"}
@@ -22,18 +24,20 @@ export default function LoginPage() {
                        value={email}
                        onChange={(e) => {
                            setEmail(e.target.value)
-                       }}/>
+                       }}
+                />
+
                 <input type="password" placeholder={"Password"}
                        className="w-full border rounded-lg px-3 py-2"
                        value={password}
                        onChange={(e) => {
                            setPassword(e.target.value)
-                       }}/>
-                <button type='submit'
-                        className="w-full bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">Login
+                       }}
+                />
+                <button type='submit' className="w-full bg-blue-600 text-white rounded-lg px-4 py-2 hover:bg-blue-700">
+                    Login
                 </button>
             </form>
-
 
         </div>
     );
